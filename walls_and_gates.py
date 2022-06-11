8d795772bf6778bc0ebbd35d613450a267721256
Kei Fukutani <kifinewera@gmail.com>
2022-06-11 12:30:29 +0900

    update

--- walls_and_gates.py
+++ walls_and_gates.py
@@ -5,15 +5,43 @@ from helper_classes import *
 import numpy as np
 import unittest
 
+from collections import deque
+
 
 class Solution:
+    '''
+    BFS
+    '''
+
     def __init__(self):
         pass
 
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
-        """
-        Do not return anything, modify rooms in-place instead.
-        """
+        GATE = 0
+        # down, up, left, right
+        DIR = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+        if rooms is None:
+            return []
+        # Initialize the queue with all 0s.
+        R, C = len(rooms), len(rooms[0])
+        # Put all the gates first so that we do BFS one at a time for each gate.
+        q = deque()
+        for r in range(R):
+            for c in range(C):
+                if rooms[r][c] == GATE:
+                    q.append((r, c))
+
+        while q:
+            r, c = q.popleft()
+            for dr, dc in DIR:
+                nr = r + dr
+                nc = c + dc
+                # rooms[nr][nc] > rooms[r][c] means that,
+                # next square is empty or the other gate is farther than this gate.
+                if 0 <= nr < R and 0 <= nc < C and rooms[nr][nc] > rooms[r][c]:
+                    rooms[nr][nc] = rooms[r][c] + 1
+                    q.append((nr, nc))
 
 
 class TestSolution(unittest.TestCase):
