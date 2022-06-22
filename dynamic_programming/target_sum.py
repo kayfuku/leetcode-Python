359119f2adcb0a457bc6c0c6c9dc459c1b3c8ee9
Kei Fukutani <kifinewera@gmail.com>
2022-06-22 10:27:11 +0900

    update

--- dynamic_programming/target_sum.py
+++ dynamic_programming/target_sum.py
@@ -13,17 +13,19 @@ class Solution:
         pass
 
     def findTargetSumWays(self, A, S):
+        # Initialize with 0 as key and its count 1 as value.
         count = Counter({0: 1})
         for x in A:
-            step = Counter()
-            # count is the previous step which keeps track of the number of
+            next = Counter()
+            # 'count' is the previous 'next' which keeps track of the number of
             # ways for each key.
             for y in count:
                 # y + x and y - x take over the number of ways for y.
-                step[y + x] += count[y]
-                step[y - x] += count[y]
+                next[y + x] += count[y]
+                next[y - x] += count[y]
 
-            count = step
+            count = next
+            print(count)
 
         return count[S]
 
@@ -35,36 +37,36 @@ class TestSolution(unittest.TestCase):
         Test
         '''
         input_and_expected_outputs = [
-            # (input, input, expected output) depending on number of arguments
-            ([0, 1, 2], 3, 6),
-            ([0, 1], 3, 5),
+            # (input1, input2, expected output) depending on number of arguments
+            ([10, 5, 5], 10, 3),  # Fail
+            ([10, 5, 5], 10, 2),
         ]
         s = Solution()
         for input1, input2, expected in input_and_expected_outputs:
-            with self.subTest(input1=input1, input2=input2):
-                result = s.solve(input1, input2)
+            with self.subTest(input1=input1, input2=input2, expected=expected):
+                result = s.findTargetSumWays(input1, input2)
                 self.assertEqual(result, expected)
 
-    def test_tree(self):
-        '''
-        Tree test example
-        '''
-        n1 = TreeNode(5)
-        n2 = TreeNode(1)
-        n3 = TreeNode(5)
-        n4 = TreeNode(5)
-        n5 = TreeNode(5)
-        n6 = TreeNode(5)
+    # def test_tree(self):
+    #     '''
+    #     Tree test example
+    #     '''
+    #     n1 = TreeNode(5)
+    #     n2 = TreeNode(1)
+    #     n3 = TreeNode(5)
+    #     n4 = TreeNode(5)
+    #     n5 = TreeNode(5)
+    #     n6 = TreeNode(5)
 
-        n1.left = n2
-        n1.right = n3
-        n3.right = n6
-        n2.left = n4
-        n2.right = n5
+    #     n1.left = n2
+    #     n1.right = n3
+    #     n3.right = n6
+    #     n2.left = n4
+    #     n2.right = n5
 
-        s = Solution()
-        result = s.countUnivalSubtrees(n1)
-        self.assertEqual(result, 4)
+    #     s = Solution()
+    #     result = s.countUnivalSubtrees(n1)
+    #     self.assertEqual(result, 4)
 
 
 def main():
