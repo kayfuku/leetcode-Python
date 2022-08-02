d32c4c57ec0d842d243b54e01c032ac7c8d29081
Kei Fukutani <kifinewera@gmail.com>
2022-08-02 12:29:15 +0900

    update

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,6 +1,5 @@
 # Author: leetcode + kei
 # Date: July 30, 2022
-from turtle import back
 from typing import *
 from helper_classes import *
 from collections import *
@@ -10,6 +9,7 @@ import unittest
 
 class Solution:
     '''
+    The next solution is better for interview
     '''
 
     def __init__(self):
@@ -112,12 +112,10 @@ class Solution:
         backtrack()
 
 
-class Solution2:
+class Solution:
     '''
+    Use set, AC
     '''
-
-    def __init__(self):
-        pass
 
     def solveSudoku(self, board):
         """
@@ -163,7 +161,10 @@ class Solution2:
 
             return nr, nc
 
-        def backtrack(r, c):
+        def backtrack(r=0, c=0):
+            if board[r][c] != '.':
+                r, c = move_to_next(r, c)
+
             # Iterate over all numbers from 1 to 9.
             for d in range(1, 10):
                 # (Constraints)
@@ -204,9 +205,104 @@ class Solution2:
                     place_number(d, i, j)
 
         # Let's get started.
-        # TODO: the first cell is not filled.
-        nr, nc = move_to_next(0, 0)
-        backtrack(nr, nc)
+        backtrack()
+
+
+class Solution2:
+    '''
+    Use set, NG! I don't know why.
+    '''
+
+    def __init__(self):
+        pass
+
+    def solveSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: void Do not return anything, modify board in-place instead.
+        """
+        def could_place(d, row, col):
+            """
+            Check if one could place a number d in (row, col) cell
+            """
+            return not (d in rows[row] or d in columns[col] or
+                        d in boxes[box_index(row, col)])
+
+        def place_number(d, row, col):
+            """
+            Place a number d in (row, col) cell
+            """
+            rows[row].add(d)
+            columns[col].add(d)
+            boxes[box_index(row, col)].add(d)
+            board[row][col] = str(d)
+
+        def remove_number(d, row, col):
+            """
+            Remove a number which didn't lead
+            to a solution
+            """
+            rows[row].remove(d)
+            columns[col].remove(d)
+            boxes[box_index(row, col)].remove(d)
+            board[row][col] = '.'
+
+        def move_to_next(r, c):
+            if c == N - 1:
+                nr = r + 1
+                nc = 0
+            else:
+                nr = r
+                nc = c + 1
+
+            return nr, nc
+
+        def backtrack(r=0, c=0):
+            while r != N - 1 and c != N - 1 and board[r][c] != '.':
+                r, c = move_to_next(r, c)
+
+            if r == N - 1 and c == N - 1:
+                # We're in the last cell, that means we have the solution.
+                return True
+
+            # Iterate over all numbers from 1 to 9.
+            for d in range(1, 10):
+                # (Constraints)
+                if not could_place(d, r, c):
+                    continue
+
+                # (Candidate)
+                place_number(d, r, c)
+                nr, nc = move_to_next(r, c)
+                if backtrack(nr, nc):  # (Explore further)
+                    return True
+
+                # Recover. (Backtracking)
+                remove_number(d, r, c)
+
+            return False
+
+        # box size
+        n = 3
+        # row size
+        N = n * n
+        # lambda function to compute box index
+        def box_index(row, col): return (row // n) * n + col // n
+
+        # Use set for each row, column, and box to check if
+        # there is a number already placed in there.
+        rows = [set() for i in range(N)]
+        columns = [set() for i in range(N)]
+        boxes = [set() for i in range(N)]
+        # Initialize the dictionaries.
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] != '.':
+                    d = int(board[i][j])
+                    place_number(d, i, j)
+
+        # Let's get started.
+        backtrack()
 
 
 class TestSolution(unittest.TestCase):
@@ -217,13 +313,29 @@ class TestSolution(unittest.TestCase):
         '''
         input_and_expected_outputs = [
             # (input1, input2, expected output) depending on number of arguments
-            ([0, 1, 2], 3, 6),
-            ([0, 1], 3, 5),
+            ([["5", "3", ".", ".", "7", ".", ".", ".", "."],
+              ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+              [".", "9", "8", ".", ".", ".", ".", "6", "."],
+              ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+              ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+              ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+              [".", "6", ".", ".", ".", ".", "2", "8", "."],
+              [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+              [".", ".", ".", ".", "8", ".", ".", "7", "9"]],
+             [["5", "3", "4", "6", "7", "8", "9", "1", "2"],
+              ["6", "7", "2", "1", "9", "5", "3", "4", "8"],
+              ["1", "9", "8", "3", "4", "2", "5", "6", "7"],
+              ["8", "5", "9", "7", "6", "1", "4", "2", "3"],
+              ["4", "2", "6", "8", "5", "3", "7", "9", "1"],
+              ["7", "1", "3", "9", "2", "4", "8", "5", "6"],
+              ["9", "6", "1", "5", "3", "7", "2", "8", "4"],
+              ["2", "8", "7", "4", "1", "9", "6", "3", "5"],
+              ["3", "4", "5", "2", "8", "6", "1", "7", "9"]]),
         ]
-        s = Solution()
-        for input1, input2, expected in input_and_expected_outputs:
-            with self.subTest(input1=input1, input2=input2, expected=expected):
-                result = s.solve(input1, input2)
+        s = Solution2()
+        for input1, expected in input_and_expected_outputs:
+            with self.subTest(input1=input1, expected=expected):
+                result = s.solveSudoku(input1)
                 self.assertEqual(result, expected)
 
     # def test_tree(self):
