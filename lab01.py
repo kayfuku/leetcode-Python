0b7ce19caa58b4c84ba6a53745d508d4899857fe
Kei Fukutani <kifinewera@gmail.com>
2022-08-03 11:03:14 +0900

    update

--- lab01.py
+++ lab01.py
@@ -8,20 +8,6 @@ from collections import deque
 
 def main():
     # experiment code
-    a = [1, 2]
-    b = []
-    b.append(a)
-    print(b)
-    a += [3]
-    print(b)
-    b.append(a)
-    print(b)
-    a.pop()
-    print(b)
-    b.append(a[:])  # Create a new list containing elements in list a.
-    print(b)
-    a += [3]
-    print(b)
 
     # done
     print('done')
