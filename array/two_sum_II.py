c2ac5ed9aceb64da78928c401af2c0643bd423ad
Kei Fukutani <kifinewera@gmail.com>
2022-08-07 11:51:25 +0900

    update

--- array/two_sum_II.py
+++ array/two_sum_II.py
@@ -17,8 +17,17 @@ class Solution:
         pass
 
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            sum = numbers[left] + numbers[right]
+            if sum == target:
+                return [left + 1, right + 1]
+            if sum < target:
+                left += 1
+            else:
+                right -= 1
 
-        return 0
+        return [-1, -1]
 
 
 class TestSolution(unittest.TestCase):
