84e7cb0c31fa7591a83a299ae5c4da99bde5be07
Kei Fukutani <kifinewera@gmail.com>
2022-08-08 12:24:34 +0900

    update

---
+++ hash_table/two_sum_III.py
@@ -0,0 +1,106 @@
+# Author: leetcode + kei
+# Date: August 8, 2022
+from typing import *
+from helper_classes import *
+from collections import *
+import numpy as np
+import unittest
+
+
+class Solution:
+    '''
+    Note:
+    There could be duplicate numbers.
+    '''
+
+    def __init__(self):
+        self.counts = dict()
+
+    def add(self, number: int) -> None:
+        # dict.get(key, default)
+        # Code pattern to count something.
+        self.counts[number] = self.counts.get(number, 0) + 1
+
+    def find(self, value: int) -> bool:
+        for num in self.counts.keys():
+            comp = value - num
+            if num == comp and self.counts.get(num) > 1:
+                # There are two or more 'num' in 'counts'.
+                return True
+            if num != comp and self.counts.get(comp, 0) > 0:
+                # There exists at least one comp in the 'counts'.
+                return True
+
+        return False
+
+# Your TwoSum object will be instantiated and called as such:
+# obj = TwoSum()
+# obj.add(number)
+# param_2 = obj.find(value)
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_solve(self):
+        '''
+        Test
+        '''
+        input_and_expected_outputs = [
+            # (input1, input2, expected output) depending on number of arguments
+            ([0, 1, 2], 3, 6),
+            ([0, 1], 3, 5),
+        ]
+        s = Solution()
+        for input1, input2, expected in input_and_expected_outputs:
+            with self.subTest(input1=input1, input2=input2, expected=expected):
+                result = s.solve(input1, input2)
+                self.assertEqual(result, expected)
+
+    # def test_tree(self):
+    #     '''
+    #     Tree test example
+    #     '''
+    #     # Binary Tree
+    #     #     6
+    #     #    /  \
+    #     #   3    12
+    #     #  / \   / \
+    #     # 1   4 9  14
+
+    #     n1 = TreeNode(6)
+    #     n2 = TreeNode(3)
+    #     n3 = TreeNode(12)
+    #     n4 = TreeNode(1)
+    #     n5 = TreeNode(4)
+    #     n6 = TreeNode(9)
+    #     n7 = TreeNode(14)
+
+    #     n1.left = n2
+    #     n1.right = n3
+    #     n2.left = n4
+    #     n2.right = n5
+    #     n3.left = n6
+    #     n3.right = n7
+
+    #     s = Solution()
+    #     input_and_expected_outputs = [
+    #         # (input1, input2, expected output) depending on number of arguments
+    #         (n1, n6, n7, n3),
+    #         (n1, n3, n5, n1),
+    #         (n1, n4, n5, n1),  # Fail
+    #     ]
+    #     s = Solution()
+    #     for input1, input2, input3, expected in input_and_expected_outputs:
+    #         with self.subTest(input1=input1.val, input2=input2.val, input3=input3.val,
+    #                           expected=expected.val):
+    #             result = s.solve(input1, input2, input3)
+    #             self.assertEqual(result, expected)
+
+
+def main():
+    """ For testing """
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
