2e13896bffc7ac5d2a9289aad3df9bb020af7c78
Kei Fukutani <kifinewera@gmail.com>
2022-08-24 10:25:06 +0900

    update

--- squares_of_sorted_array.py
+++ squares_of_sorted_array.py
@@ -15,8 +15,22 @@ class Solution:
         pass
 
     def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [0] * n
+        left = 0
+        right = n - 1
+        # Put the squares in the 'result' from right to left.
+        for i in range(n - 1, -1, -1):
+            if abs(nums[left]) < abs(nums[right]):
+                square = nums[right]
+                right -= 1
+            else:
+                square = nums[left]
+                left += 1
 
-        return 0
+            result[i] = square * square
+
+        return result
 
 
 class TestSolution(unittest.TestCase):
