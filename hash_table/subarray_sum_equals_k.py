e112c4fe070311dfc9664be162325b6c123e5ec4
Kei Fukutani <kifinewera@gmail.com>
2022-09-07 10:16:15 +0900

    update

--- hash_table/subarray_sum_equals_k.py
+++ hash_table/subarray_sum_equals_k.py
@@ -1,26 +1,32 @@
 # Author: vijayzuzu + kei
-# Date: July 21, 2021
+# Date: July 21, 2021, September 6, 2022
 import unittest
 from typing import *
 from helper_classes import *
 
 
 class Solution:
-    def subarraySum(self, nums: List[int], k: int) -> int:
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        count = 0
-        sums = 0
-        d = dict()
-        d[0] = 1
+    '''
+    cum_sum A - cum_sum B = sum of subarray
+    '''
 
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0
+        cum_sum = 0
+        # K: cum_sum, V: occurances of cum_sums
+        d = dict()
+        # Don't forget this!
+        # Cumulative sum 0 is needed if the subarray starts from the beginning.
+        d[0] = 1
         for i in range(len(nums)):
-            sums += nums[i]
-            count += d.get(sums - k, 0)
-            d[sums] = d.get(sums, 0) + 1
+            # Cumulative sum so far
+            cum_sum += nums[i]
+            # If the subarray whose sum is cum_sum - k exists, then
+            # the subarray whose sum is k exists.
+            count += d.get(cum_sum - k, 0)
+            # Count the number of specific sums.
+            # Since nums[i] can be negative, same cum_sum can show up multiple times.
+            d[cum_sum] = d.get(cum_sum, 0) + 1
 
         return count
 
@@ -33,13 +39,12 @@ class TestSolution(unittest.TestCase):
         '''
         input_and_expected_outputs = [
             # (input1, input2, expected output) depending on number of arguments
-            ([0, 1, 2], 3, 6),
-            ([0, 1], 3, 5),
+            ([5, 6, 7], 1, 0),
         ]
         s = Solution()
         for input1, input2, expected in input_and_expected_outputs:
-            with self.subTest(input1=input1, input2=input2, expected=expected):
-                result = s.solve(input1, input2)
+            with self.subTest(input1=input1, k=input2, expected=expected):
+                result = s.subarraySum(input1, input2)
                 self.assertEqual(result, expected)
 
     # def test_tree(self):
