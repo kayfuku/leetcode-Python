e1af1f5ffd773e6fb75c683f289d4be405142067
Kei Fukutani <kifinewera@gmail.com>
2022-09-12 12:17:32 +0900

    update

--- bst/validatet_binary_search_tree.py
+++ bst/validatet_binary_search_tree.py
@@ -1,30 +1,29 @@
 # Author: leetcode + kei
-# Date: July 21, 2021
+# Date: July 21, 2021, September 12, 2022
 import unittest
 from typing import *
 from helper_classes import *
 
-import math
-
 
 class Solution:
 
-    def validate(self, node, low=None, high=None):
-        # Empty trees are valid BSTs.
-        if node is None:
-            return True
-        # The current node's value must be between low and high.
-        if low is not None and node.val <= low:
-            return False
-        if high is not None and node.val >= high:
-            return False
-
-        # The left and right subtree must also be valid.
-        return (self.validate(node.left, low, node.val) and
-                self.validate(node.right, node.val, high))
-
     def isValidBST(self, root: TreeNode) -> bool:
-        return self.validate(root)
+
+        def validate(node, low=None, high=None):
+            # Empty trees are valid BSTs.
+            if node is None:
+                return True
+            # The current node's value must be between low and high.
+            if low is not None and node.val <= low:
+                return False
+            if high is not None and node.val >= high:
+                return False
+
+            # The left and right subtree must also be valid.
+            return validate(node.left, low, node.val) and \
+                validate(node.right, node.val, high)
+
+        return validate(root)
 
 
 class TestSolution(unittest.TestCase):
