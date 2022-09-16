239208a1ae16f2a81e345b6be8669623a89f94ce
Kei Fukutani <kifinewera@gmail.com>
2022-09-16 11:58:45 +0900

    update

--- graph_dfs_bfs/number_of_islands.py
+++ graph_dfs_bfs/number_of_islands.py
@@ -1,13 +1,16 @@
 # Author: girikuncoro + kei
 # Date: July 24, 2021, September 14, 2022
+from logging.handlers import QueueListener
 import unittest
 from typing import *
 from helper_classes import *
+from collections import *
 
 
 class Solution:
     '''
     1. DFS
+    O(MN) time and space, where M is the number of rows and N is the number of coloumns.
     '''
 
     def numIslands(self, grid: List[List[str]]) -> int:
@@ -43,13 +46,55 @@ class Solution:
 class Solution2:
     '''
     2. BFS
+    O(MN) time, O(M + N) space
     '''
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        def bfs(r, c):
+            q = deque()
+            q.append([r, c])
+            grid[r][c] = VISITED
+            while q:
+                size = len(q)
+                for i in range(size):
+                    r, c = q.popleft()
+                    for d in D:
+                        nr = r + d[0]
+                        nc = c + d[1]
+                        if nr < 0 or nr >= R or nc < 0 or nc >= C or \
+                                grid[nr][nc] == WATER or grid[nr][nc] == VISITED:
+                            continue
+                        q.append([nr, nc])
+                        grid[nr][nc] = VISITED
+
+        if not grid:
+            return 0
+
+        D = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        WATER = '0'
+        ISLAND = '1'
+        VISITED = '2'
+        R = len(grid)
+        C = len(grid[0])
+        count = 0
+        for row in range(R):
+            for col in range(C):
+                if grid[row][col] == ISLAND:
+                    bfs(row, col)
+                    count += 1
+
+        return count
 
 
 class Solution3:
     '''
     3. Union Find
     '''
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        return 0
 
 
 class TestSolution(unittest.TestCase):
