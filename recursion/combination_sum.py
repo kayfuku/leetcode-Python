d2a7c5e54dfbfa181cdad5ca4e7cb3e274e6ec47
Kei Fukutani <kifinewera@gmail.com>
2022-10-16 18:18:05 +0900

    update

---
+++ recursion/combination_sum.py
@@ -0,0 +1,104 @@
+# Author: leetcode + kei
+# Date: October 16, 2022
+from typing import *
+from helper_classes import *
+from collections import *
+import numpy as np
+import unittest
+
+
+class Solution:
+    '''
+    '''
+
+    def combinationSum(self, candidates: List[int],
+                       target: int) -> List[List[int]]:
+
+        def combinations(start, remaining):
+            if remaining == 0:
+                output.append(temp_list[:])
+                return
+            if remaining < 0:
+                return
+
+            for i in range(start, n):
+                if candidates[i] > remaining:
+                    continue
+                temp_list.append(candidates[i])
+                # Caution! 'i' here, not 'start'!
+                combinations(i, remaining - candidates[i])
+                temp_list.pop()
+
+        output = []
+        temp_list = []
+        n = len(candidates)
+        combinations(0, target)
+        return output
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_solve(self):
+        '''
+        Test
+        '''
+        input_and_expected_output = [
+            # (input1, input2, expected output) depending on number of arguments
+            ([2, 3, 6, 7], 7, [[2, 2, 3], [7]]),
+        ]
+        s = Solution()
+        for case, (input1, input2, expected) in enumerate(
+                input_and_expected_output):
+            print('Case: {}'.format(case))
+            with self.subTest(input1=input1, input2=input2, expected=expected):
+                result = s.combinationSum(input1, input2)
+                self.assertEqual(result, expected)
+
+    # def test_tree(self):
+    #     '''
+    #     Tree test example
+    #     '''
+    #     # Binary Tree
+    #     #     6
+    #     #    /  \
+    #     #   3    12
+    #     #  / \   / \
+    #     # 1   4 9  14
+
+    #     n1 = TreeNode(6)
+    #     n2 = TreeNode(3)
+    #     n3 = TreeNode(12)
+    #     n4 = TreeNode(1)
+    #     n5 = TreeNode(4)
+    #     n6 = TreeNode(9)
+    #     n7 = TreeNode(14)
+
+    #     n1.left = n2
+    #     n1.right = n3
+    #     n2.left = n4
+    #     n2.right = n5
+    #     n3.left = n6
+    #     n3.right = n7
+
+    #     s = Solution()
+    #     input_and_expected_outputs = [
+    #         # (input1, input2, expected output) depending on number of arguments
+    #         (n1, n6, n7, n3),
+    #         (n1, n3, n5, n1),
+    #         (n1, n4, n5, n1),  # Fail
+    #     ]
+    #     s = Solution()
+    #     for input1, input2, input3, expected in input_and_expected_outputs:
+    #         with self.subTest(input1=input1.val, input2=input2.val, input3=input3.val,
+    #                           expected=expected.val):
+    #             result = s.solve(input1, input2, input3)
+    #             self.assertEqual(result, expected)
+
+
+def main():
+    """ For testing """
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
