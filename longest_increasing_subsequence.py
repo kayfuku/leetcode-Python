a73ef8a94b486c67356e47f229124c6e9aecad34
Kei Fukutani <kifinewera@gmail.com>
2022-10-18 00:16:29 +0900

    update

--- longest_increasing_subsequence.py
+++ longest_increasing_subsequence.py
@@ -9,11 +9,18 @@ import unittest
 
 class Solution:
     '''
+    Dynamic Programming
+
     '''
 
     def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [1] * len(nums)
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
 
-        return 0
+        return max(dp)
 
 
 class TestSolution(unittest.TestCase):
