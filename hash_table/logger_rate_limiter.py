6bccfc5600de78063771a42ce69874765fcf6ced
Kei Fukutani <kifinewera@gmail.com>
2022-11-17 11:03:28 +0900

    update

--- hash_table/logger_rate_limiter.py
+++ hash_table/logger_rate_limiter.py
@@ -9,23 +9,17 @@ import unittest
 class Logger:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
+        # K: message, V: next allowed timestamp
         self._msg_dict = {}
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        """
-        Returns true if the message should be printed in the given timestamp, otherwise returns false.
-        If this method returns false, the message will not be printed.
-        The timestamp is in seconds granularity.
-        """
         if message not in self._msg_dict:
-            self._msg_dict[message] = timestamp
+            self._msg_dict[message] = timestamp + 10
             return True
 
-        if timestamp - self._msg_dict[message] >= 10:
-            self._msg_dict[message] = timestamp
+        next_allowed_time = self._msg_dict[message]
+        if timestamp >= next_allowed_time:
+            self._msg_dict[message] = timestamp + 10
             return True
 
         return False
