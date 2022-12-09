1c6722ca98f0d6ac066dd1c0dde44aeffbfb1e2d
Kei Fukutani <kifinewera@gmail.com>
2022-12-09 11:35:18 +0900

    update

---
+++ graph_dfs_bfs_union_find/all_paths_from_source_to_target.py
@@ -0,0 +1,124 @@
+# Author: leetcode + kei
+# Date: December 9, 2022
+from typing import *
+from helper_classes import *
+from collections import defaultdict, deque
+from sortedcontainers import SortedList, SortedSet, SortedDict
+import bisect
+import heapq
+import numpy as np
+import unittest
+
+
+class Solution:
+    '''
+    Backtracking
+    '''
+
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        target = len(graph) - 1
+        results = []
+
+        def backtrack(curr_node, path):
+            # if we reach the target, no need to explore further.
+            if curr_node == target:
+                results.append(list(path))
+                return
+
+            # explore the neighbor nodes one after another.
+            for next_node in graph[curr_node]:
+                path.append(next_node)
+                seen.add(next_node)
+                backtrack(next_node, path)
+                path.pop()
+                seen.remove(next_node)
+
+        # kick of the backtracking, starting from the source node (0).
+        path = [0]
+        seen = set()
+        backtrack(0, path)
+
+        return results
+
+
+class Try:
+
+    def solve(self, nums: List[int], target: int) -> List[int]:
+        return 0
+
+
+class Bot:
+
+    def solve(self, nums: List[int], target: int) -> List[int]:
+        return 0
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_solve(self):
+        '''
+        Test
+        Change input and expected output as needed.
+        '''
+        input_and_expected_output = [
+            # (input1, input2, expected output) depending on number of arguments
+            ([0, 1, 2], 3, 6),
+            ([0, 1], 3, 5),
+        ]
+        s = Solution()
+        for case, (input1, input2, expected) in enumerate(
+                input_and_expected_output):
+            print('Case: {}'.format(case))
+            with self.subTest(input1=input1, input2=input2, expected=expected):
+                # Change to the method name to be tested.
+                result = s.topKFrequent(input1, input2)
+                self.assertEqual(result, expected)
+
+    # def test_tree(self):
+    #     '''
+    #     Tree test example
+    #     '''
+    #     # Binary Tree
+    #     #     6
+    #     #    /  \
+    #     #   3    12
+    #     #  / \   / \
+    #     # 1   4 9  14
+
+    #     n1 = TreeNode(6)
+    #     n2 = TreeNode(3)
+    #     n3 = TreeNode(12)
+    #     n4 = TreeNode(1)
+    #     n5 = TreeNode(4)
+    #     n6 = TreeNode(9)
+    #     n7 = TreeNode(14)
+
+    #     n1.left = n2
+    #     n1.right = n3
+    #     n2.left = n4
+    #     n2.right = n5
+    #     n3.left = n6
+    #     n3.right = n7
+
+    #     s = Solution()
+    #     input_and_expected_outputs = [
+    #         # (input1, input2, expected output) depending on number of arguments
+    #         (n1, n6, n7, n3),
+    #         (n1, n3, n5, n1),
+    #         (n1, n4, n5, n1),  # Fail
+    #     ]
+    #     s = Solution()
+    #     for input1, input2, input3, expected in input_and_expected_outputs:
+    #         with self.subTest(input1=input1.val, input2=input2.val, input3=input3.val,
+    #                           expected=expected.val):
+    #             result = s.solve(input1, input2, input3)
+    #             self.assertEqual(result, expected)
+
+
+def main():
+    """ For testing """
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
