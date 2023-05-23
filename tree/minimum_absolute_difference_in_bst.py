bfbf27e1c08e005ad1b0bcaa44395090f08505d6
Kei Fukutani <kifinewera@gmail.com>
2023-05-23 16:06:34 +0900

    update

---
+++ tree/minimum_absolute_difference_in_bst.py
@@ -0,0 +1,138 @@
+# Author: leetcode + kei
+# Date: May 23, 2023
+from typing import *
+from helper_classes import *
+from collections import defaultdict, deque
+from sortedcontainers import SortedList, SortedSet, SortedDict
+import bisect
+import heapq
+import numpy as np
+from functools import cache
+import unittest
+
+
+class Solution:
+    '''
+    BST, in-order traversal.
+    '''
+
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        inorderNodes = []
+
+        def inorder(node):
+            if node is None:
+                return
+            inorder(node.left)
+            inorderNodes.append(node.val)
+            inorder(node.right)
+
+        inorder(root)
+        minDifference = 1e9
+        for i in range(1, len(inorderNodes)):
+            minDifference = min(minDifference, inorderNodes[i] - inorderNodes[i-1])
+
+        return minDifference
+
+    def getMinimumDifference2(self, root: Optional[TreeNode]) -> int:
+        # use self.min_ and self.prev_val or nonlocal min_ and prev_val in inorder()
+        self.min_ = float('inf')
+        self.prev_val = None
+
+        def inorder(node):
+            # nonlocal min_, prev_val
+            if node is None:
+                return
+            inorder(node.left)
+            if self.prev_val is not None:
+                self.min_ = min(self.min_, node.val - self.prev_val)
+            self.prev_val = node.val
+            inorder(node.right)
+
+        inorder(root)
+        return self.min_
+
+
+class Try:
+
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+
+        return 0
+
+
+class Bot:
+
+    def solve(self, nums: List[int], target: int) -> List[int]:
+        return 0
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_solve(self):
+        '''
+        Test
+        Change input and expected output as needed.
+        '''
+        input_and_expected_output = [
+            # (input1, input2, expected output) depending on number of arguments
+            ([0, 1, 2], 3, 6),
+            ([0, 1], 3, 5),
+        ]
+        s = Solution()
+        for case, (input1, input2, expected) in enumerate(
+                input_and_expected_output):
+            print('Case: {}'.format(case))
+            with self.subTest(input1=input1, input2=input2, expected=expected):
+                # Change to the method name to be tested.
+                result = s.topKFrequent(input1, input2)
+                self.assertEqual(result, expected)
+
+    # def test_tree(self):
+    #     '''
+    #     Tree test example
+    #     '''
+    #     # Binary Tree
+    #     #     6
+    #     #    /  \
+    #     #   3    12
+    #     #  / \   / \
+    #     # 1   4 9  14
+
+    #     n1 = TreeNode(6)
+    #     n2 = TreeNode(3)
+    #     n3 = TreeNode(12)
+    #     n4 = TreeNode(1)
+    #     n5 = TreeNode(4)
+    #     n6 = TreeNode(9)
+    #     n7 = TreeNode(14)
+
+    #     n1.left = n2
+    #     n1.right = n3
+    #     n2.left = n4
+    #     n2.right = n5
+    #     n3.left = n6
+    #     n3.right = n7
+
+    #     s = Solution()
+    #     input_and_expected_outputs = [
+    #         # (input1, input2, expected output) depending on number of arguments
+    #         (n1, n6, n7, n3),
+    #         (n1, n3, n5, n1),
+    #         (n1, n4, n5, n1),  # Fail
+    #     ]
+    #     s = Solution()
+    #     for input1, input2, input3, expected in input_and_expected_outputs:
+    #         with self.subTest(input1=input1.val, input2=input2.val, input3=input3.val,
+    #                           expected=expected.val):
+    #             result = s.solve(input1, input2, input3)
+    #             self.assertEqual(result, expected)
+
+
+def main():
+    """ For testing """
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
