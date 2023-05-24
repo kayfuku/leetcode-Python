3e02eb9a76701581945612bf41bd71534a042823
Kei Fukutani <kifinewera@gmail.com>
2023-05-24 17:27:56 +0900

    update

---
+++ dynamic_programming/maximal_square.py
@@ -0,0 +1,117 @@
+# Author: leetcode + kei
+# Date: May 24, 2023
+from typing import *
+from helper_classes import *
+from collections import defaultdict, deque
+from sortedcontainers import SortedList, SortedSet, SortedDict
+import bisect
+import heapq
+import numpy as np
+from functools import cache
+import unittest
+
+
+class Solution:
+    '''
+    '''
+
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if matrix is None or len(matrix) < 1:
+            return 0
+
+        R = len(matrix)
+        C = len(matrix[0])
+
+        dp = [[0] * (C+1) for _ in range(R+1)]
+        max_side = 0
+        for r in range(R):
+            for c in range(C):
+                if matrix[r][c] == '1':
+                    # Be careful of the indexing since dp grid has additional row and column
+                    dp[r+1][c+1] = min(dp[r][c], dp[r+1][c], dp[r][c+1]) + 1
+                    max_side = max(max_side, dp[r+1][c+1])
+
+        return max_side * max_side
+
+
+class Try:
+
+    def solve(self, nums: List[int], target: int) -> List[int]:
+        return 0
+
+
+class Bot:
+
+    def solve(self, nums: List[int], target: int) -> List[int]:
+        return 0
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_solve(self):
+        '''
+        Test
+        Change input and expected output as needed.
+        '''
+        input_and_expected_output = [
+            # (input1, input2, expected output) depending on number of arguments
+            ([0, 1, 2], 3, 6),
+            ([0, 1], 3, 5),
+        ]
+        s = Solution()
+        for case, (input1, input2, expected) in enumerate(
+                input_and_expected_output):
+            print('Case: {}'.format(case))
+            with self.subTest(input1=input1, input2=input2, expected=expected):
+                # Change to the method name to be tested.
+                result = s.topKFrequent(input1, input2)
+                self.assertEqual(result, expected)
+
+    # def test_tree(self):
+    #     '''
+    #     Tree test example
+    #     '''
+    #     # Binary Tree
+    #     #     6
+    #     #    /  \
+    #     #   3    12
+    #     #  / \   / \
+    #     # 1   4 9  14
+
+    #     n1 = TreeNode(6)
+    #     n2 = TreeNode(3)
+    #     n3 = TreeNode(12)
+    #     n4 = TreeNode(1)
+    #     n5 = TreeNode(4)
+    #     n6 = TreeNode(9)
+    #     n7 = TreeNode(14)
+
+    #     n1.left = n2
+    #     n1.right = n3
+    #     n2.left = n4
+    #     n2.right = n5
+    #     n3.left = n6
+    #     n3.right = n7
+
+    #     s = Solution()
+    #     input_and_expected_outputs = [
+    #         # (input1, input2, expected output) depending on number of arguments
+    #         (n1, n6, n7, n3),
+    #         (n1, n3, n5, n1),
+    #         (n1, n4, n5, n1),  # Fail
+    #     ]
+    #     s = Solution()
+    #     for input1, input2, input3, expected in input_and_expected_outputs:
+    #         with self.subTest(input1=input1.val, input2=input2.val, input3=input3.val,
+    #                           expected=expected.val):
+    #             result = s.solve(input1, input2, input3)
+    #             self.assertEqual(result, expected)
+
+
+def main():
+    """ For testing """
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
