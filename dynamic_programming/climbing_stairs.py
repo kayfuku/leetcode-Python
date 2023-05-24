eeb057459b6344173f76d2fb9fb6773899f70f18
Kei Fukutani <kifinewera@gmail.com>
2023-05-24 14:32:48 +0900

    update

--- dynamic_programming/climbing_stairs.py
+++ dynamic_programming/climbing_stairs.py
@@ -24,6 +24,17 @@ class Solution:
             dp[i] = dp[i-1] + dp[i-2]
         return dp[-1]
 
+    def climbStairs2_2(self, n: int) -> int:
+        if n == 1:
+            return 1
+        dp = [0 for i in range(n+1)]
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+
+    # The best solution.
     # Bottom up, O(1) space
     def climbStairs3(self, n: int) -> int:
         if n == 1:
